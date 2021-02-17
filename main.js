c4e6faf1fd3fbe7b671071a8d69ddd77b73b2ad0
Renee Onque <reneeonque@Renees-MBP.home>
2021-02-16 20:22:36 -0500

    Learned to make a calculator

---
+++ main.js
@@ -0,0 +1,11 @@
+function age_calculator(member, name, age) {
+	return `My ${member} ${name} is ${age*12} months old!`;
+}
+
+age_calculator("mother", "Karima", "45");
+age_calculator("father", "Suliman", "46");
+age_calculator("oldest sister", "Sharifa", "29");
+age_calculator("brother", "Rahhiem", "27");
+age_calculator("sister", "Nafeesa", "25");
+age_calculator("sister", "Faiza", "25");
+age_calculator("youngest sister", "Kamryn", "9");
